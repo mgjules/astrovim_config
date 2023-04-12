3b18bb6c84f9ecfca9d0582c32b098a72a5bb0c0
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2023-04-12 17:33:52 +0400

    chore: styling

--- plugins/user.lua
+++ plugins/user.lua
@@ -11,13 +11,9 @@ return {
   -- },
   {
     "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    version = "*",
     event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup {
-        -- Configuration here, or leave empty to use defaults
-      }
-    end,
+    config = function() require("nvim-surround").setup {} end,
   },
   {
     "iamcco/markdown-preview.nvim",
