8c1e051849a46c560ddb4eac379c7d9a5579ac53
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2023-04-15 21:25:22 +0400

    feat: add leap

--- plugins/user.lua
+++ plugins/user.lua
@@ -14,6 +14,19 @@ return {
     version = "*",
     event = "VeryLazy",
     config = function() require("nvim-surround").setup {} end,
+    keys = {
+      { "cs", "<Plug>Csurround", desc = "change surrounding #1 by #2" },
+      { "ds", "<Plug>Dsurround", desc = "delete surrounding #1" },
+      { "cS", "<Plug>CSurround", desc = "change surrounding #1 by #2 + new line" },
+      { "ys", "<Plug>Ysurround", desc = "wraps #1 in #2 (surround)" },
+      { "yS", "<Plug>YSurround", desc = "wraps #1 in #2 (surround) + new line" },
+      { "yss", "<Plug>Yssurround", desc = "wraps line in #1 (surround)" },
+      { "ySs", "<Plug>YSsurround", desc = "wraps line in #1 (surround) + new line" },
+      { "ySS", "<Plug>YSsurround", desc = "wraps line in #1 (surround) + new line" },
+      { "gs", "<Plug>VSurround", desc = "wraps visual selection in #1 (surround)", mode = "x" },
+      { "gS", "<Plug>VgSurround", desc = "wraps visual selection in #1 (surround) + new line", mode = "x" },
+    },
+    init = function() vim.g.surround_no_mappings = 1 end,
   },
   {
     "iamcco/markdown-preview.nvim",
@@ -42,5 +55,11 @@ return {
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+  {
+    "ggandor/leap.nvim",
+    dependencies = { "tpope/vim-repeat" },
+    event = { "BufRead" },
+    config = function() require("leap").add_default_mappings(true) end,
   },
 }
